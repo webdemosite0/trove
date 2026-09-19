52b9fc1284d1e4a1f1cff4a9395d1618d2a2a1f9
webdemosite0 <webdemosite0@gmail.com>
2026-09-20 00:49:49 +0500

    Remove All work section from all shell pages

--- src/app/(shell)/layout.tsx
+++ src/app/(shell)/layout.tsx
@@ -11,8 +11,6 @@ import { isMobile } from "@/lib/device";
 import { countDueReminders } from "@/app/actions/reminders";
 import { listAllRecents } from "@/lib/recents";
 import { AnnouncementBanner } from "@/components/shell/announcement-banner";
-import { AllWorkSection } from "@/components/work/all-work-section";
-import { AllWorkVisibility } from "@/components/work/all-work-visibility";
 
 export const metadata: Metadata = {
   robots: { index: false, follow: false },
@@ -63,12 +61,7 @@ export default async function ShellLayout({
           <main className="flex min-w-0 flex-1 flex-col">
             <TopBar initial={user?.name?.slice(0, 1)} due={due} />
             <AnnouncementBanner />
-            <div className="app-page-in min-w-0 flex-1">
-              {children}
-              <AllWorkVisibility>
-                <AllWorkSection limit={18} />
-              </AllWorkVisibility>
-            </div>
+            <div className="app-page-in min-w-0 flex-1">{children}</div>
           </main>
         </div>
       </ToastProvider>
