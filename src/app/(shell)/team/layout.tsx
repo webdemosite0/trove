b88ccc0fcf18f42b144b3cc8b2784f795cd84fce
webdemosite0 <webdemosite0@gmail.com>
2026-09-25 00:48:12 +0500

    Remove duplicate Team layout imports

--- src/app/(shell)/team/layout.tsx
+++ src/app/(shell)/team/layout.tsx
@@ -1,8 +1,6 @@
 import { TeamNav } from "@/components/team/team-nav";
 import { currentUser } from "@/lib/auth";
 import { teamForUser } from "@/lib/team";
-import { currentUser } from "@/lib/auth";
-import { teamForUser } from "@/lib/team";
 
 export default async function TeamAreaLayout({
   children,
